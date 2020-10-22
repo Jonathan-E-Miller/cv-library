0a6af967db5af02c8694a6b47024ad93e8b09f65
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-10-22 16:40:55 +0100

    Database created if does not exist
    The database is now created if it does not exist.
    The ASP identity tables are created
    The CvDocument table is created which is linked to a user.

--- CvWebApi/Database/ApplicationDbContext.cs
+++ CvWebApi/Database/ApplicationDbContext.cs
@@ -5,11 +5,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CvWebApi.Models;
 
 namespace CvWebApi.Database
 {
   public class ApplicationDbContext : IdentityDbContext<IdentityUser>
   {
+    public DbSet<CvDocument> CvDocuments { get; set; }
+
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
 
--- CvWebApi/Startup.cs
+++ CvWebApi/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CvWebApi.Database;
+using CvWebApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +29,8 @@ namespace CvWebApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
+      services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
       services.AddControllers();
     }
 
@@ -46,6 +52,12 @@ namespace CvWebApi
       {
         endpoints.MapControllers();
       });
+
+      using (var service = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+      {
+        ApplicationDbContext context = service.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureCreated();
+      }
     }
   }
 }
