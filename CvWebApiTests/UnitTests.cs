f9acc0a688c442592fa11b41e400ca9cd549d302
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-10-23 15:58:33 +0100

    Unit Test for Weather API Example
    Added a unit test for making a get request to the /weatherforecast end point.

--- CvWebApiTests/UnitTests.cs
+++ CvWebApiTests/UnitTests.cs
@@ -1,8 +1,15 @@
 using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using CvWebApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Collections;
+using CvWebApi;
+using System.Collections.Generic;
 
 namespace CvWebApiTests
 {
-  public class Tests
+  public class UnitTests
   {
     [SetUp]
     public void Setup()
@@ -10,9 +17,14 @@ namespace CvWebApiTests
     }
 
     [Test]
-    public void Test1()
+    public void TestWeather()
     {
-      Assert.Pass();
+      Mock<ILogger<WeatherForecastController>> mockLogger = new Mock<ILogger<WeatherForecastController>>();
+      WeatherForecastController wfc = new WeatherForecastController(mockLogger.Object);
+
+      IEnumerable<WeatherForecast> forcasts = wfc.Get();
+
+      Assert.IsNotNull(forcasts);
     }
   }
 }
