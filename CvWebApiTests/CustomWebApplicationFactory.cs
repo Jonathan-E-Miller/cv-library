eee7742d2e5df04afd6131e2b534e5a63d6db711
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-10-24 17:37:43 +0100

    Added Integration Tests
    Added integration testing for the Web API. The application is simulated using an extension of WebApplicationFactory as per recommended technique via Microsoft docs.
    
    An in memory database is created to model Entity Framework.
    
    Test for /weatherforecast has been written to prove that it is working and ready for development.

---
+++ CvWebApiTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+﻿using CvWebApi;
+using CvWebApi.Database;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace CvWebApiTests
+{
+  public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+  {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+      builder.ConfigureServices(services =>
+      {
+        // Create a new service provider.
+        var serviceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+        // Add a database context (AppDbContext) using an in-memory database for testing.
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+          options.UseInMemoryDatabase("InMemoryAppDb");
+          options.UseInternalServiceProvider(serviceProvider);
+        });
+
+        // Build the service provider.
+        var sp = services.BuildServiceProvider();
+
+        // Create a scope to obtain a reference to the database contexts
+        using (var scope = sp.CreateScope())
+        {
+          var scopedServices = scope.ServiceProvider;
+          var appDb = scopedServices.GetRequiredService<ApplicationDbContext>();
+
+          var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+          // Ensure the database is created.
+          appDb.Database.EnsureCreated();
+
+          // TODO Seed data here.
+        }
+      });
+    }
+  }
+}
