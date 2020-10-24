001e392c4f0b11b4112fa71b74776e3620ce1c94
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-10-24 17:37:43 +0100

    Added Integration Tests
    Added integration testing for the Web API. The application is simulated using an extension of WebApplicationFactory as per recommended technique via Microsoft docs.
    
    An in memory database is created to model Entity Framework.
    
    Test for /weatherforecast has been written to prove that it is working and ready for development.

--- CvWebApiTests/IntegrationTests.cs
+++ CvWebApiTests/IntegrationTests.cs
@@ -1,21 +1,39 @@
-﻿using NUnit.Framework;
+﻿using Castle.Core.Internal;
+using CvWebApi;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CvWebApiTests
 {
   public class IntegrationTests
   {
-    [SetUp]
-    public void Setup()
+    private CustomWebApplicationFactory<CvWebApi.Startup> _factory;
+    private HttpClient _client;
+
+    [OneTimeSetUp]
+    public void CreateClientFromFactory()
     {
+      _factory = new CustomWebApplicationFactory<Startup>();
+      _client = _factory.CreateClient();
     }
 
-    [Test]
-    public void Test1()
+
+    [TestCase("/weatherforecast")]
+    public async Task TestEndPointReturnsContent(string endpoint)
     {
-      Assert.Pass();
+      // The endpoint or route of the controller action.
+      var httpResponse = await _client.GetAsync(endpoint);
+
+      var result = httpResponse.Content;
+
+      string content = result.ReadAsStringAsync().Result;
+
+      // make sure that the content was returned.
+      Assert.IsFalse(content.IsNullOrEmpty());
     }
   }
 }
