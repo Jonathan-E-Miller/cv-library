e951db4c3265be84187bc88058d75b88bce36efe
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-11-26 14:32:13 +0000

    Upgraded to .NET Core 5.0
    Upgraded to the latest version of .NET Core so we are using the latest version.

--- CvWebApi/Models/Company.cs
+++ CvWebApi/Models/Company.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,7 +12,6 @@ namespace CvWebApi.Models
   {
     public int CompanyId { get; set; }
     
-    [Index(IsUnique = true)]
     [StringLength(50)]
     public string CompanyName { get; set; }
     public Sector Sector { get; set; }
--- CvWebApi/Models/Subject.cs
+++ CvWebApi/Models/Subject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,7 +12,6 @@ namespace CvWebApi.Models
   {
     public int SubjectId { get; set; }
 
-    [Index(IsUnique = true)]
     [StringLength(50)]
     public string Name { get; set; }
   }
