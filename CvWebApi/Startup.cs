28e43786f59447812e321f59e0c1099cc8d5b8a7
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-11-27 12:55:59 +0000

    CORS allow credentials
    Allow the cookie to contain cookie credentials when coming from localhost:4200.

--- CvWebApi/Startup.cs
+++ CvWebApi/Startup.cs
@@ -42,7 +42,7 @@ namespace CvWebApi
         options.AddPolicy(name: MyAllowSpecificOrigins,
                           builder =>
                           {
-                            builder.WithOrigins("http://localhost:4200");
+                            builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowCredentials();
                           });
       });
 
