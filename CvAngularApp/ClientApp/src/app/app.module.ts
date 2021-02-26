75a884ae1ebf77ec8a5c38db6ae7c7ee763c1ea5
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2021-02-26 11:47:52 +0000

    Redirect to login
    Redirects to login if a cookie does not exist. This resolves #31

--- CvAngularApp/ClientApp/src/app/app.module.ts
+++ CvAngularApp/ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { LoginComponent } from './login/login.component';
       { path: 'login', component: LoginComponent }
     ])
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
